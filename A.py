04fafee2c4318b2447d397f6b822ebe00eed8926
Jessica Tran <35790773+JessTran7@users.noreply.github.com>
2020-02-13 13:21:14 -0500

    Update A.py

--- A.py
+++ A.py
@@ -1,165 +1,232 @@
-import math
+import heapq as heap
+from math import sqrt
 
-#Global variable to keep track of the final path
-point_path = []
-final_x = 0
-final_y = 0
+#Uses a heap to implement and store a priority queue
+class PQ():
+    def __init__(self):
+        self.queue = []
+        self.map = {}
+        self.delete = 'deleted'
+
+    def push(self, task, pri = 0):
+        if task in self.map:
+            self.remove(task)
+        entry = [pri, task]
+        self.map[task] = entry
+        heap.heappush(self.queue, entry)
+    
+    def remove(self, task):
+        entry = self.map.pop(task)
+        entry[-1] = self.delete
+    
+    def pop(self):
+        while self.queue:
+            pri, task = heap.heappop(self.queue)
+            if task is not self.delete:
+                del self.map[task]
+                return (pri, task)
 
 
-#Node for the path
-class Node:
-    def __init__(self, loc=None, x=None, y=None):
-        self.x = x
-        self.y = y
-        self.loc = loc
-        self.distance_End = 0
-        self.path_to_here = 0
-        self.next = []
-visited = {}
-BreakTheWorld = False
+class Map():
+    def __init__(self, con_file, loc_file):
+        self.locate = {}
+        self.connect ={}
+        #Maps a node to its location
+        loc = open(loc_file, 'r')
+        for x in loc:
+            line = x.rstrip('\n')#rstrip
+            formatted_line = line.split()
+            self.locate[formatted_line[0]]= formatted_line[1:]
+        del self.locate["END"] 
+        #Maps a node to its connection
+        con = open(con_file, 'r')
+        for x in con:
+            line = x.rstrip('\n')#rstrip
+            formatted_line = line.split()
+            self.connect[formatted_line[0]]= formatted_line[2:]
+        del self.connect["END"]
+        #close files
+        loc.close()
+        con.close()
+
+    def excludes(self, node):
+        if node not in self.connect.keys():
+            print (node + " doesn't exist")
+            return -1
+        exclude = self.connect[node]
+        del self.connect[node]
+        del self.locate[node]
+
+        for x in exclude:
+            lists = self.connect[x]
+            lists.remove(node)
+
+    #0 is straight line distance and 1 is fewest cities.
+    def calc_heur(self, node, end, heur):
+        if heur == 1:
+            return 1
+        else:
+            return self.calc(node, end)
+    
+    #Calculation for a straight line distance
+    def calc(self, a, b):
+        loc1 = self.locate[a]
+        x1 = int(loc1[0])
+        y1 = int(loc1[1])
+        loc2 = self.locate[b]
+        x2 = int(loc2[0])
+        y2 = int(loc2[1])
+        return sqrt((x2-x1)**2 + (y2-y1)**2)
+
+    
+    def print_Path(self, final_path, heur):
+        if final_path == []:
+            print("No path")
+        else:
+            final_path.reverse()
+            total = 0
+
+            for x in range(0,len(final_path)-1):
+                if heur == 0:
+                    dist = self.calc(final_path[x], final_path[x+1])
+                    total += dist
+                    print(final_path[x] + " to " + final_path[x+1] + " length is %.1f" % dist)
+                else:
+                    total += 1
+                    print(final_path[x] + " to " + final_path[x+1] + " length 1")
+            print("Total path length = %.1f" % total)
+
+    def make_Path(self, prev, current):
+        path = [current]
+        while current in prev.keys():
+            current = prev[current]
+            path.append(current)
+        return path
+
+    def as_algorithm(self, start, end, heur, step):
+        #Set initial values and variables
+        prev_map = {}
+        open_nodes = PQ()
+        closed_nodes = []
+        cost = {}
+        cost[start] = 0 
+        total_cost = {}
+        cost_estimate = self.calc_heur(start, end, heur)
+        #Pushes start node onto queue
+        open_nodes.push(start, cost_estimate)
+        total_cost[start] = cost_estimate
+
+        #Open nodes aren't empty
+        while open_nodes.map:
+            current = open_nodes.pop()
+            current_node = current[1]
+
+            if step:
+                print("\nSelected city: " + current_node)
+
+            if current_node == end:
+                return self.make_Path(prev_map, current_node)
+
+            closed_nodes.append(current_node)
+            neighbors = self.connect[current_node]
+            if step:
+                print("Possible cities: %s" % neighbors)
+
+            for neighbor in neighbors:
+                if neighbor in closed_nodes:
+                    continue
+                #Calculate cost to neighbor
+                if heur:
+                    cost_neighbor = 1 + cost[current_node]
+                else:
+                    cost_neighbor = self.calc(current_node, neighbor) + cost[current_node]
+
+                #Calculate heuristic cost estimate from neighbor to end
+                cost_estimate = self.calc_heur(neighbor, end, heur)
+                total_cost_neighbor = cost_estimate + cost_neighbor
+                #Checks to see if there is a better path
+                if neighbor in open_nodes.map.keys():
+                    if total_cost_neighbor >= total_cost[neighbor]:
+                        continue
+                #Else add neighbor to queue and update values
+                cost[neighbor] = cost_neighbor
+                open_nodes.push(neighbor, total_cost_neighbor)
+                total_cost[neighbor] = total_cost_neighbor
+                prev_map[neighbor] = current_node
+
+            if step:
+                print("Cities at end of possible paths:", end = " ")
+                for x in open_nodes.queue:
+                    if x[1] != "deleted":
+                        print(x[1] + "(%.1f)," % x[0], end = ' ')
+                print("\n")
+                print("****************************************************************************************")
+
+        #No path existed
+        return []  
 
 
 def main():
-    #Opening and reading file locations.txt until EOF
-    f = open('locations.txt', 'r')
-    file = f.readlines()
-    #Initializing variables
-    locations = []
-    Nodes = []
-    ID = 0
-    #Format the two txt files (not changing the file itself but how its received) so that it is easier to use
-    for lines in file:
-        line = lines.rstrip('\n')
-        line = line.split(' ')
-        loc = line[0]
-        if loc == 'END':
-            break
-        x = line[1]
-        y = line[2]
-        Nodes.append(Node(loc, x, y))
-        locations.append(loc)
-        visited[loc] = False
-        ID += 1
-    #Closing file locations.txt
-    f.close()
-    
-    #Opening and reading file connections.txt until EOF
-    f = open('connections.txt', 'r')
-    file = f.readlines()
-    for lines in file:
-        line = lines.rstrip('\n')
-        line = line.split(' ')
-        loc = line[0]
-        if loc == 'END':
-            break
-        count = int(line[1])
-        
-        for k in range(count+1):
-            for x in range(count+1):
-                if (x == 0):
-                    continue
+    #Initalize a map
+    map = Map("connections.txt", "locations.txt")
 
-                elif (x == 1):
-                    continue
-                #Format connections alphabetically
-                elif (line[x] > line[x+1]):
-                    temp = line[x]
-                    line[x] = line[x+1]
-                    line[x+1] = temp
-
-        for i in range(count):
-            x = locations.index(line[i + 2])
-            Nodes[locations.index(loc)].next.append(Nodes[x])
-    #Closing file connections.txt
-    f.close()
-
-    #Get input from user to enter in starting and final locations.
-    print("Please enter in the starting location:", end = " ")
-    start = input()
-    #Input validation
-    while start not in locations:
-        print("Invalid starting location. Please try again:", end = " ")
-        start = input()
-    print("Please enter in the final location:", end = " ")
-    end = input()
-    #Input validation
-    while end not in locations:
-        print("Invalid final location. Please try again:", end = " ")
-        end = input()
-    final_x = Nodes[locations.index(end)].x
-    final_y = Nodes[locations.index(end)].y
-
-    print(final_x + " " + final_y)
-    Calculate_distance(Nodes, final_x, final_y)
-    for Part in Nodes:
-        print(Part.loc + " " + str(Part.distance_End))
-    path = [start]*len(locations)
-    length = [0]*len(locations)
-
-    #Start the path and print the path
-    findPath(Nodes[locations.index(start)], path, end, length, 0, 0)
-    for x in range(len(point_path)):
-        print(point_path[len(point_path)-x-1])
-
-    #Prints total length of path
-    print('\n'"The total length of the path is " + str(tot_length))
-    
-
-
-def findPath(node,path, end, length, count, final_distance): 
-    if node.loc == end:
-        global BreakTheWorld
-        BreakTheWorld = True
-        global tot_length
-        tot_length = final_distance
-        return 
-    if visited[node.loc]:
-        return
-    else:
-        visited[node.loc] = True
-    #####################################################################################################
-    #create the node list and tell the code which nodes come first with respect to the estimated distance
-    move = []
-    for y in node.next:
-        move.append(y)
-    for z in range(0, len(move)-1):
-        for x in range(0, len(move)-1):
-            move[x].path_to_here = move[x].distance_End + final_distance + distance(node.x, node.y, move[x].x, move[x].y)
-            move[x+1].path_to_here = move[x+1].distance_End + final_distance + distance(node.x, node.y, move[x].x, move[x].y)
-            if move[x].path_to_here > move[x+1].path_to_here:
-                tmp = move[x+1]
-                move[x+1] = move[x]
-                move[x] = tmp
-    print("The order for " + str(node.loc) + " to check is: ")
-    for order in move:
-        print(order.loc + " with current distance " + str(final_distance + distance(node.x, node.y, order.x, order.y)) + " with estimated distance: " + str(order.path_to_here))
-    print("\n")
-    ######################################################################################################
-
-    for tmp in node.next:
-        #Find the right path via DFS constraints given
-        final_distance += distance(node.x, node.y, tmp.x, tmp.y)
-        count += 1
-        findPath(tmp, path, end, length, count, final_distance)
-        #If the end has been located
-        if BreakTheWorld:
-            point_path.append(node.loc + " to "  + tmp.loc + " length is " + str(distance(node.x, node.y, tmp.x, tmp.y)) + "    the estimated distance to the end from Node is: " + str(node.distance_End))
-            break
+    #User input for starting node and input validation
+    while 1:
+        print("Please enter in the starting location:", end = " ")
+        begin = str(input())
+        if begin not in map.connect.keys():
+            print("Incorrect selection, please try again.\n")
         else:
-            final_distance -= distance(node.x, node.y, tmp.x, tmp.y)
+            break
+    #User input for end node and input validation
+    while 1:
+        print("\nPlease enter in the final location:", end = " ")
+        end = str(input())
+        if end not in map.connect.keys():
+            print ("Incorrect selection, please try again.")
+        else:
+            break
 
-#Math function to calculate the distance between two individual nodes
-def distance(x1,y1,x2,y2):
-    x = math.pow(int(x1)-int(x2), 2)
-    y = math.pow(int(y1)-int(y2), 2)
-
-    return math.sqrt(x+y)
-
-def Calculate_distance(Nodes,final_x, final_y):
-    for part in Nodes:
-        part.distance_End = distance(part.x, part.y, final_x, final_y)
-
-
-
+    #Allows user to exclude 1 or more cities
+    print("\nWould you like to exclude any cities (yes or no)?", end = " ")
+    answer = str(input())
+    if answer == "yes":
+        print("\nPlease enter in the nodes you wish to bypass seperated by a space:", end = " ")
+        excluding = (input())
+        #Input validation
+        if excluding not in map.connect.keys():
+            print("Incorrect selection, please try again:", end = " ")
+            excluding = (input())
+            exclude = excluding.split()
+        else:
+            exclude = excluding.split()
+        for x in exclude:
+            map.excludes(x)
+    else:
+        answer = "no"
+            
+    
+    print("\nStraight line distance (0) or fewest cities (1)?:", end = " ")
+    heur = int(input())
+    while heur != 1 and heur != 0:
+        print("Error try again (0 or 1):", end = " ")
+        heur = int(input())
+    
+    print("\nWould you like to see a step by step solution (yes or no)?:", end = " ")
+    step = str(input())
+    while step != "yes" and step != "no":
+        print("Error try again (yes or no):", end = " ")
+        step = str(input())
+    if step == "yes":
+        step = True
+    else:
+        step = False
+    
+    #Prints final path
+    final_path = map.as_algorithm(begin, end, heur, step)
+    print("----------------------------------------------------------------------------------------")
+    print("----------------------------------------------------------------------------------------")
+    print("The final solution path is:")
+    map.print_Path(final_path, heur)
 
 main()
